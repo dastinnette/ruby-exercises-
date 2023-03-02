0c0d4d913120dfe750b8d84369da4c182a94e2b9
David Stinnette <dastinnette@gmail.com>
2023-03-02 15:38:23 -0500

    Passing tests 3 through 10

--- ruby_basics/6_arrays/spec/array_exercises_spec.rb
+++ ruby_basics/6_arrays/spec/array_exercises_spec.rb
@@ -4,7 +4,7 @@ require_relative '../exercises/array_exercises'
 RSpec.describe 'Array Exercises' do
   describe 'nil array exercise' do
 
-    it 'returns an array containing 5 nil values' do
+    xit 'returns an array containing 5 nil values' do
       expect(nil_array(5)).to eq([nil, nil, nil, nil, nil])
     end
 
@@ -16,45 +16,45 @@ RSpec.describe 'Array Exercises' do
 
   describe 'first element exercise' do
 
-    xit 'returns the first element of an array of numbers' do
+    it 'returns the first element of an array of numbers' do
       expect(first_element([2, 4, 6, 8, 10])).to eq(2)
     end
 
-    xit 'returns the first element of an array of strings' do
+    it 'returns the first element of an array of strings' do
       expect(first_element(['foo', 'bar'])).to eq('foo')
     end
   end
 
   describe 'third element exercise' do
 
-    xit 'returns the third element of an array of numbers' do
+    it 'returns the third element of an array of numbers' do
       expect(third_element([2, 4, 6, 8, 10])).to eq(6)
     end
 
-    xit 'returns nil if the array does not have a third element' do
+    it 'returns nil if the array does not have a third element' do
       expect(third_element(['foo', 'bar'])).to eq(nil)
     end
   end
 
   describe 'last three elements exercise' do
 
-    xit 'returns an array of the last three elements' do
+    it 'returns an array of the last three elements' do
       expect(last_three_elements([2, 4, 6, 8, 10])).to eq([6, 8, 10])
     end
 
-    xit 'returns all of the elements when there are less than 3 elements' do
+    it 'returns all of the elements when there are less than 3 elements' do
       expect(last_three_elements(['foo', 'bar'])).to eq(['foo', 'bar'])
     end
   end
 
   describe 'add element exercise' do
 
-    xit 'increases the length of an array by 1' do
+    it 'increases the length of an array by 1' do
       numbers = [1, 2, 3, 4]
       expect { add_element(numbers) }.to change { numbers.length }.by(1)
     end
 
-    xit 'increases the length of an empty array by 1' do
+    it 'increases the length of an empty array by 1' do
       data = []
       expect { add_element(data) }.to change { data.length }.by(1)
     end
